c0b57fde875a92ce44d03b1c51a6d5e3d7cee60b
rhmsarr <sarr.rahma@gmx.com>
2024-12-14 13:36:22 +0300

    updated the Ingredient index, now takes the data from the db, instead of getting it from the ingredientservice.cs. ingredientservice is now useless, deleted it.

--- WhatsForDinner/Controllers/IngredientController.cs
+++ WhatsForDinner/Controllers/IngredientController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WhatsForDinner.Models;
 
 namespace WhatsForDinner.Controllers{
 
     public class IngredientController : Controller{
 
+        private readonly DinnerDbContext _context;
+
+        public IngredientController(DinnerDbContext context){
+            _context = context;
+        }
+
+
         [Route("MyAccount/MyPantry")]
         public IActionResult Index(){
-            return View(IngredientService.ingredients);
+            var ingredients = _context.Ingredients.Include(i => i.IngredientCategory).AsQueryable();
+            List<Ingredient> ingredientsList = ingredients.OrderBy(i => i.IngredientCategory.Name).ToList();
+            return View(ingredientsList);
         }
 
         public IActionResult SaveIngredients(List<Ingredient> model){
