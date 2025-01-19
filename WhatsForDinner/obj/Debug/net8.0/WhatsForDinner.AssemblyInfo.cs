280ada51c8384e26bfae34f66331f6a6e817d13b
rhmsarr <sarr.rahma@gmx.com>
2025-01-19 13:06:50 +0300

    modified appsettings.json

--- WhatsForDinner/obj/Debug/net8.0/WhatsForDinner.AssemblyInfo.cs
+++ WhatsForDinner/obj/Debug/net8.0/WhatsForDinner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsForDinner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402ae94c6bc7094fdd915a5525ec9803cbc2fcaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301f68a8220264757d86d091eadec29172ea5cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsForDinner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsForDinner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
